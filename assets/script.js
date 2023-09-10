6f95a2108fec79b5782c6a01d7f523078adfef81
Rodolfo <rodolfoespinosa01@gmail.com>
2023-09-09 21:48:32 -0400

    functioning app, pending README file to work on

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -9,6 +9,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
